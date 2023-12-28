8ac1e0d41e6ed7a251f8a99d00196557fb95de53
kimcuong <cuongnguyen.javadev@gmail.com>
2023-12-28 23:49:39 +0700

    config MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,68 @@
-function App() {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+  useColorScheme
+} from '@mui/material'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import SettingsSystemDaydreamIcon from '@mui/icons-material/SettingsSystemDaydream'
 
+const ModeSystem = () => {
+  const { mode, setMode } = useColorScheme()
+  const handleChange = (event: any) => {
+    setMode(event.target.value)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size='small'>
+      <InputLabel id='label-select-dark-light-mode'>Mode</InputLabel>
+      <Select
+        labelId='label-select-dark-light-mode'
+        id='select-dark-light-mode'
+        value={mode}
+        label='Mode'
+        onChange={handleChange}
+      >
+        <MenuItem value={'light'}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <LightModeIcon fontSize='small' />
+            Light
+          </Box>
+        </MenuItem>
+        <MenuItem value={'system'}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <SettingsSystemDaydreamIcon fontSize='small' />
+            System
+          </Box>
+        </MenuItem>
+        <MenuItem value={'dark'}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeIcon fontSize='small' />
+            Dark
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
+function App() {
   return (
     <>
       <div>
         <p>đây là page số 1</p>
       </div>
+      <Typography variant='body2' color='text.secondary'>
+        Test theming
+      </Typography>
+      <ModeSystem />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.scss";
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import { CssBaseline } from '@mui/material'
+import theme from './theme'
+import App from './App'
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <>
+    <CssVarsProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </CssVarsProvider>
+  </>
+)
